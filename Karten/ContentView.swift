a585b027a4f06d944a948f5046c4ba100a071f28
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-03-19 15:43:57 +0100

    created Xcode project

---
+++ Karten/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Karten
+//
+//  Created by Leon on 2024/3/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Karten/KartenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KartenApp.swift
+//  Karten
+//
+//  Created by Leon on 2024/3/19.
+//
+
+import SwiftUI
+
+@main
+struct KartenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
